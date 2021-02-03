11e354a7b5044e08cdf2f51907752a58797f8bdb
alessfm <11_aleXS@protonmail.com>
2021-02-03 11:27:19 -0300

    testing scss and electron-reload(is working)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const isDev = !app.isPackaged;
 
 function createWindow(){
     const win = new BrowserWindow({
-        width: 980,
+        width: 490,
         height: 640,
         backgroundColor: 'white',
         icon: '',
@@ -19,12 +19,27 @@ function createWindow(){
 
     win.loadFile('index.html');
 }
+
+ // options menu when user left-click on icon
+app.setUserTasks([
+    {
+      program: process.execPath,
+      arguments: '--new-window',
+      iconPath: process.execPath,
+      iconIndex: 0,
+      title: 'New Window',
+      description: 'Create a new window'
+    }
+  ])
+
+  // to update on changes
 if (isDev){
     require('electron-reload')(__dirname,{
         electron: path.join(__dirname,'node-modules','.bin','electron')
     })
 }
 
+ // to send notifications to Windows Users
 ipcMain.on('notify',(e,message)=>{
     new Notification({title:'Ei!',body: message}).show();
 })
--- src/js/Card.js
+++ src/js/Card.js
@@ -3,12 +3,13 @@ import React from 'react';
 export default function Card(){
     return(
         <div className="card">
-            <img src='../img/'></img>
+            <img src=''></img>
             <div className="content">
                 <h3>The Candle Curse</h3>
                 <button onClick={()=>{
                     electron.notificationApi.sendNotification(
-                        'Iniciando o jogo!');}}>Jogar</button>
+                        'Iniciando o jogo!');}}>Jogar
+                </button>
             </div>
         </div>
     )
